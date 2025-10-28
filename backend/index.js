c16c0e1ebd878502a1fc4572c731da211136c221
Abhishek Gaur <agaur0136@gmail.com>
2025-10-28 12:16:37 +0530

    connected to database

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,18 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Database is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 const app = express();
 
 // middleware to handle cors
@@ -15,7 +25,7 @@ app.use(
 );
 
 // Middleware to handle JSON object in req body
-app.use(express.json())
+app.use(express.json());
 
 const port = process.env.PORT || 8080;
 
